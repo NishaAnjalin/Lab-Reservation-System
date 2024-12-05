2ee1dc89c5b921a1656b3c9ec0f1a222422ad124
NishaAnjalin <139215902+NishaAnjalin@users.noreply.github.com>
2024-12-05 15:16:55 +0000

    Time table Page created

---
+++ pages/timetable.php
@@ -0,0 +1,180 @@
+<html>
+<head>
+    <title>Default Time Table</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+</head>
+<body class="bg-white-900 text-black">
+    
+        <!-- Main Content -->
+        <div class="flex-1 bg-white-800 p-4">
+            <div class="flex justify-between items-center mb-4">
+                <div class="flex space-x-2">
+                    <button class="bg-blue-500 text-white px-4 py-2 rounded-full">CL 1</button>
+                    <button class="bg-gray-300 text-gray-800 px-4 py-2 rounded-full">CL 2</button>
+                </div>
+                <h1 class="text-xl font-bold">Default Time Table</h1>
+                <button class="bg-gray-300 text-gray-800 px-4 py-2 rounded-full flex items-center">
+                    <i class="fas fa-plus mr-2"></i> ADD NEW
+                </button>
+            </div>
+            <div class="bg-gray-100 text-gray-800 p-4 rounded-lg">
+                <div class="grid grid-cols-6 gap-4">
+                    
+                    <div class="col-span-5 grid grid-cols-5 gap-4">
+                        <table class="table-auto">
+                            <tr>
+                               <th>Monday</th>
+                               <th>Tuesday</th> 
+                               <th>Wednesday</th> 
+                               <th>Thursday</th> 
+                               <th>Friday</th> 
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div class="bg-white p-2 rounded-lg border-l-4 border-green-500">
+                                        <div class="flex justify-between">
+                                            <span>CSC3122</span>
+                                            <i class="fas fa-caret-down"></i>
+                                        </div>
+                                        <div>CL 1</div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="bg-white p-2 rounded-lg border-l-4 border-green-500">
+                                        <div class="flex justify-between">
+                                            <span>CSC3122</span>
+                                            <i class="fas fa-caret-down"></i>
+                                        </div>
+                                        <div>CL 1</div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="bg-white p-2 rounded-lg border-l-4 border-green-500">
+                                        <div class="flex justify-between">
+                                            <span>CSC3122</span>
+                                            <i class="fas fa-caret-down"></i>
+                                        </div>
+                                        <div>CL 1</div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="bg-white p-2 rounded-lg border-l-4 border-green-500">
+                                        <div class="flex justify-between">
+                                            <span>CSC3122</span>
+                                            <i class="fas fa-caret-down"></i>
+                                        </div>
+                                        <div>CL 1</div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="bg-white p-2 rounded-lg border-l-4 border-green-500">
+                                        <div class="flex justify-between">
+                                            <span>CSC3122</span>
+                                            <i class="fas fa-caret-down"></i>
+                                        </div>
+                                        <div>CL 1</div>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div class="bg-white p-2 rounded-lg border-l-4 border-green-500">
+                                        <div class="flex justify-between">
+                                            <span>CSC3122</span>
+                                            <i class="fas fa-caret-down"></i>
+                                        </div>
+                                        <div>CL 1</div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <!-- <div class="bg-white p-2 rounded-lg border-l-4 border-green-500">
+                                        <div class="flex justify-between">
+                                            <span>CSC3122</span>
+                                            <i class="fas fa-caret-down"></i>
+                                        </div>
+                                        <div>CL 1</div>
+                                    </div> -->
+                                </td>
+                                <td>
+                                    <div class="bg-white p-2 rounded-lg border-l-4 border-green-500">
+                                        <div class="flex justify-between">
+                                            <span>CSC3122</span>
+                                            <i class="fas fa-caret-down"></i>
+                                        </div>
+                                        <div>CL 1</div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="bg-white p-2 rounded-lg border-l-4 border-green-500">
+                                        <div class="flex justify-between">
+                                            <span>CSC3122</span>
+                                            <i class="fas fa-caret-down"></i>
+                                        </div>
+                                        <div>CL 1</div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <!-- <div class="bg-white p-2 rounded-lg border-l-4 border-green-500">
+                                        <div class="flex justify-between">
+                                            <span>CSC3122</span>
+                                            <i class="fas fa-caret-down"></i>
+                                        </div>
+                                        <div>CL 1</div>
+                                    </div> -->
+                                </td>
+                            </tr>
+                            <tr></tr>
+                                <td>
+                                    <div class="bg-white p-2 rounded-lg border-l-4 border-green-500">
+                                        <div class="flex justify-between">
+                                            <span>CSC3122</span>
+                                            <i class="fas fa-caret-down"></i>
+                                        </div>
+                                        <div>CL 1</div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="bg-white p-2 rounded-lg border-l-4 border-green-500">
+                                        <div class="flex justify-between">
+                                            <span>CSC3122</span>
+                                            <i class="fas fa-caret-down"></i>
+                                        </div>
+                                        <div>CL 1</div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="bg-white p-2 rounded-lg border-l-4 border-green-500">
+                                        <div class="flex justify-between">
+                                            <span>CSC3122</span>
+                                            <i class="fas fa-caret-down"></i>
+                                        </div>
+                                        <div>CL 1</div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="bg-white p-2 rounded-lg border-l-4 border-green-500">
+                                        <div class="flex justify-between">
+                                            <span>CSC3122</span>
+                                            <i class="fas fa-caret-down"></i>
+                                        </div>
+                                        <div>CL 1</div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="bg-white p-2 rounded-lg border-l-4 border-green-500">
+                                        <div class="flex justify-between">
+                                            <span>CSC3122</span>
+                                            <i class="fas fa-caret-down"></i>
+                                        </div>
+                                        <div>CL 1</div>
+                                    </div>
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+</body>
+</html>
